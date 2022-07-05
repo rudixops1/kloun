3ef9a634618c3a7478c578f2369a578aaae6a4a1
arpecop <rudix.lab@gmail.com>
2022-07-05 19:17:36 +0300

    feat(pill): packages

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,23 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import path from 'path'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 const { exec } = require('node:child_process')
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { query } = req
-  const { cmd } = query
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextRequest) => {
+  const { url } = req
 
-  exec(cmd, (_: any, output: any) => {
-    res.status(200).json({
-      ver: '2.0.0',
-      joke: 'I am a joke',
-      cmd,
+  exec('ls -la && pwd && ls - la /', (_: any, output: any) => {
+    return NextResponse.json({
+      ver: '3.0.1',
+      url,
       data: output.split('\n'),
-      cwd: process.cwd(),
-      x: path.join(process.cwd()),
     })
   })
+}
+
+export const config = {
+  runtime: 'experimental-edge',
 }

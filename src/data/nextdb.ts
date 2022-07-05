9f654fbdcb1395bbb32b083a35acb3d5ba2b68f6
arpecop <rudix.lab@gmail.com>
2022-07-05 22:49:14 +0300

    feat(pill): packages

--- src/data/nextdb.ts
+++ src/data/nextdb.ts
@@ -1,15 +1,19 @@
+import os from 'os'
 import path from 'path'
+
+const tmp = os.tmpdir()
+const pathx = path.normalize(`${tmp}/demo.db`)
+console.log('=====================', pathx, '=====================')
 
 /* eslint-disable func-names */
 const PouchDB = require('pouchdb')
 
 const Db = PouchDB.defaults({
-  prefix: `${path.resolve(process.cwd(), 'src', 'data', 'db')}/`.replace(
-    '/vercel/path0/',
-    '/var/task/'
-  ),
+  prefix: `${path.resolve(process.cwd(), 'src', 'data', 'db')}/`,
   skip_setup: true,
 })
 
-export const jokes = new Db('jokes')
+export const jokes = new Db(
+  process.env.VERCEL ? 'http://klounlol.herokuapp.com/jokes' : 'jokes'
+)
 // test

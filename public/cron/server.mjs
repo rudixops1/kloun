14618d138b430014f5854c722efe70fc9ccf087a
arpecop <rudix.lab@gmail.com>
2022-07-05 18:34:52 +0300

    feat(pill): packages

--- public/cron/server.mjs
+++ public/cron/server.mjs
@@ -1,6 +1,3 @@
-/* eslint-disable camelcase */
-// curl -i -X POST "https://graph.facebook.com/v14.0/me/feed?link=https%3A%2F%2Fkloun.lol%2Fjoke%2F5743b656fc4106f815f97fa6171bcd78%2F&access_token=EAAGfAboWDWABAJl3ZCInlczVGAov7Gq7IZBdP2kEdjUT4hIpv7ZAFhoTgXnPmyT2jxT6Y05e493VZCDGJmvbS2B0av5YiOcNpE5V4kKX56EhTZBhUnPQrXSc09y000NOvvTbGyHzPZBtY6I9CpIFecgHEnJEFgPzOjn7uzf2GvO9WMPGr406LMTTDHZC3sx8j4muhuUh91PUlIPTsnHEdZCR"
-
 import fetch from 'node-fetch'
 
 const token =
@@ -8,13 +5,13 @@ const token =
   'EAAGfAboWDWABAMtMB3WIS51wvjjUBDOC4wqSq2Sq40aqNYBk1IZAGz3ey4YdW0rN5NhFfawvTsXZANQZB2XwcPEX5nf5WVyJ1PZCMJ3LAyqURZCEwL77Tx5QdPTZAdnZBnHGTbBPbkLSFIu6M2DNWuwWxZB4nmnIZACbw1GVpEANkApVeenIi4iIKSvHTEOCqZAmaRMRjZBGQIMRwZDZD'
 
 fetch('https://pouchdb.herokuapp.com/jokes/_all_docs')
-  .then((res) => res.json())
-  .then((json) => {
+  .then(res => res.json())
+  .then(json => {
     const { total_rows } = json
     const settings = { limit: 1, skip: Math.floor(Math.random() * total_rows) }
     fetch('https://pouchdb.herokuapp.com/jokes/_all_docs', settings)
-      .then((res) => res.json())
-      .then((json1) => {
+      .then(res => res.json())
+      .then(json1 => {
         const { id } = json1.rows[0]
         console.log(
           `https://graph.facebook.com/v14.0/me/feed?access_token=${token}`
@@ -32,8 +29,8 @@ fetch('https://pouchdb.herokuapp.com/jokes/_all_docs')
             }),
           }
         )
-          .then((res) => res.json())
-          .then((json) => {
+          .then(res => res.json())
+          .then(json => {
             console.log(json)
           })
       })

18bbd8ab4e4a5fca3c328e271910aa839958c2d8
arpecop <rudix.lab@gmail.com>
2022-07-05 15:20:50 +0300

    feat(pill): packages

--- src/data/nextdb.ts
+++ src/data/nextdb.ts
@@ -4,8 +4,11 @@ import path from 'path'
 const PouchDB = require('pouchdb')
 
 const Db = PouchDB.defaults({
-  prefix: `${path.resolve(process.cwd(), 'src', 'data', 'db')}/`,
-  skip_setup: false,
+  prefix: `${path.resolve(process.cwd(), 'src', 'data', 'db')}/`.replace(
+    '/vercel/path0/',
+    '/var/task/'
+  ),
+  skip_setup: true,
 })
 
 export const jokes = new Db('jokes')

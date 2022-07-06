2879f3744ba08594b710b4a5d7dd9af1e2d16945
arpecop <rudix.lab@gmail.com>
2022-07-06 14:01:31 +0300

    feat(pill): packages

--- src/data/server.js
+++ src/data/server.js
@@ -1,9 +1,11 @@
 const PouchDB = require('pouchdb')
 const cors = require('cors')
+const compression = require('compression')
 const express = require('express')
 
 const app = express()
 app.use(cors())
+app.use(compression())
 
 const Db = PouchDB.defaults({ prefix: `${__dirname}/db/` })
 

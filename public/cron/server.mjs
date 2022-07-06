2abc7e100ce8a8ee3a5f4e631b4c561f2d385e9c
arpecop <rudix.lab@gmail.com>
2022-07-06 17:10:07 +0300

    feat(pill): packages

--- public/cron/server.mjs
+++ public/cron/server.mjs
@@ -1,16 +1,14 @@
 import fetch from 'node-fetch'
 
-const token =
-  process.env.FB_TOKEN ||
-  'EAAGfAboWDWABAH3TwCAapcYYIq4TLp5T71sFb99xtZAQ3PaidkKy8CFMZBUJqIpcvaltwIDCKId1X1ePt4uhTvgbocRGjZCv0xVpw9i3GqZCsnxTM4KUu9d7oi3cuZA9i3Vyoq0UatXd9hKg9f9siIJs5yz1mxPrHsSvu8zbRXzxtniSkWqd3'
+const token = process.env.FB_TOKEN
 fetch('https://pouchdb.herokuapp.com/jokes/_all_docs')
-  .then(res => res.json())
-  .then(json => {
+  .then((res) => res.json())
+  .then((json) => {
     const { total_rows } = json
     const settings = { limit: 1, skip: Math.floor(Math.random() * total_rows) }
     fetch('https://klounlol.herokuapp.com/jokes/_all_docs', settings)
-      .then(res => res.json())
-      .then(json1 => {
+      .then((res) => res.json())
+      .then((json1) => {
         const { id } = json1.rows[0]
         console.log(
           `https://graph.facebook.com/v14.0/me/feed?access_token=${token}`
@@ -28,8 +26,8 @@ fetch('https://pouchdb.herokuapp.com/jokes/_all_docs')
             }),
           }
         )
-          .then(res => res.json())
-          .then(json => {
+          .then((res) => res.json())
+          .then((json) => {
             console.log(json)
           })
       })

e05df414e864bcf96ceff6f6c0dbb73216a2bdba
arpecop <rudix.lab@gmail.com>
2022-07-08 02:41:26 +0300

    feat(pill): packages

--- src/pages/api/story/[id].ts
+++ src/pages/api/story/[id].ts
@@ -12,30 +12,29 @@ export default async function handler(
   const joke = await jokes.get(id)
 
   if (joke) {
-    const image = new Jimp(300, 533, '#1F2937')
+    const image = new Jimp(200, 200, '#1F2937')
 
     // client.photos.curated({ per_page: 1 }).then((photos) => {
-    Jimp.read(`${path.resolve('./public')}/logobottom.png`).then(
-      (background) => {
-        Jimp.loadFont(`${path.resolve('./public')}/font.fnt`)
-          .then((font) => {
-            const splitted = joke.joke.split('\n')
-            let h = 10
-            splitted.forEach((line: string) => {
-              image.print(font, 10, h, line, 290)
-              h += Jimp.measureTextHeight(font, line, 290)
-            })
-            image.blit(background, 70, 320)
-            return image.resize(1080, 1920)
+    Jimp.read(`${path.resolve('./public')}/logobottom.png`).then(() => {
+      // return background
+      Jimp.loadFont(`${path.resolve('./public')}/font.fnt`)
+        .then((font) => {
+          const splitted = joke.joke.split('\n')
+          let h = 10
+          splitted.forEach((line: string) => {
+            image.print(font, 10, h, line, 190)
+            h += Jimp.measureTextHeight(font, line, 190)
           })
-          .then((image1) => {
-            image1.getBuffer(Jimp.MIME_PNG, (_, buffer) => {
-              res.setHeader('Content-Type', 'image/png')
-              res.end(buffer)
-            })
+          // image.blit(background, 70, 320)
+          return image.resize(458, 458)
+        })
+        .then((image1) => {
+          image1.getBuffer(Jimp.MIME_PNG, (_, buffer) => {
+            res.setHeader('Content-Type', 'image/png')
+            res.end(buffer)
           })
-      }
-    )
+        })
+    })
     // })
   } else {
     res.status(404).json({ error: 'Not found' })

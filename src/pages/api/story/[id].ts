90cb18c359c2e2f40343b2158120a47b626bfce7
arpecop <rudix.lab@gmail.com>
2022-07-08 03:01:06 +0300

    feat(pill): packages

--- src/pages/api/story/[id].ts
+++ src/pages/api/story/[id].ts
@@ -12,29 +12,32 @@ export default async function handler(
   const joke = await jokes.get(id)
 
   if (joke) {
-    const image = new Jimp(200, 200, '#1F2937')
+    const image = new Jimp(200, 200, '#232427')
 
     // client.photos.curated({ per_page: 1 }).then((photos) => {
-    Jimp.read(`${path.resolve('./public')}/logobottom.png`).then(() => {
-      // return background
-      Jimp.loadFont(`${path.resolve('./public')}/font.fnt`)
-        .then((font) => {
-          const splitted = joke.joke.split('\n')
-          let h = 10
-          splitted.forEach((line: string) => {
-            image.print(font, 10, h, line, 190)
-            h += Jimp.measureTextHeight(font, line, 190)
+    Jimp.read(`${path.resolve('./public')}/logobottomsmall.png`).then(
+      (background) => {
+        // return background
+        Jimp.loadFont(`${path.resolve('./public')}/font.fnt`)
+          .then((font) => {
+            const splitted = joke.joke.split('\n')
+            let h = 50
+            splitted.forEach((line: string) => {
+              image.print(font, 10, h, line, 180)
+
+              h += Jimp.measureTextHeight(font, line, 180)
+            })
+            // image.blit(background, 10, 320)
+            return image.resize(458, 458).blit(background, 190, 12)
           })
-          // image.blit(background, 70, 320)
-          return image.resize(458, 458)
-        })
-        .then((image1) => {
-          image1.getBuffer(Jimp.MIME_PNG, (_, buffer) => {
-            res.setHeader('Content-Type', 'image/png')
-            res.end(buffer)
+          .then((image1) => {
+            image1.getBuffer(Jimp.MIME_PNG, (_, buffer) => {
+              res.setHeader('Content-Type', 'image/png')
+              res.end(buffer)
+            })
           })
-        })
-    })
+      }
+    )
     // })
   } else {
     res.status(404).json({ error: 'Not found' })

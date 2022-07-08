8a93eab751d3d4488d4b10b5d3e4bd15e38db5fc
arpecop <rudix.lab@gmail.com>
2022-07-08 03:12:52 +0300

    feat(pill): packages

--- public/cron/server.mjs
+++ public/cron/server.mjs
@@ -1,17 +1,26 @@
+/* eslint-disable camelcase */
 import fetch from 'node-fetch'
 
 const token = process.env.FB_TOKEN
-fetch('https://pouchdb.herokuapp.com/jokes/_all_docs')
+fetch('https://pouchdb.herokuapp.com/jokes/')
   .then(res => res.json())
   .then(json => {
-    const { total_rows } = json
-    const skip = Math.floor(Math.random() * total_rows)
-    fetch(`https://klounlol.herokuapp.com/jokes/_all_docs?limit=1&skip=${skip}`)
+    const { doc_count } = json
+    const skip = Math.floor(Math.random() * doc_count)
+    fetch(
+      `https://klounlol.herokuapp.com/jokes/_all_docs?limit=3&skip=${skip}&include_docs=true`
+    )
       .then(res => res.json())
       .then(json1 => {
-        console.log(json1.rows)
         const { id } = json1.rows[0]
-        console.log(id)
+        const child_attachments = json1.rows.map(row => {
+          return {
+            link: `https://kloun.lol/joke/${row.id}/`,
+            name: row.doc.cat,
+            picture: `https://kloun.lol/api/story/${row.id}/`,
+          }
+        })
+
         console.log(
           `https://graph.facebook.com/v14.0/me/feed?access_token=${token}`
         )
@@ -28,6 +37,7 @@ fetch('https://pouchdb.herokuapp.com/jokes/_all_docs')
               og_action_type_id: '383635058339457',
               og_object_id: '140035382814582',
               og_icon_id: '202857099862987',
+              child_attachments,
               actions: [
                 {
                   link: `https://kloun.lol/joke/${id}/`,

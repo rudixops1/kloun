73c52b1506e0bccbbbf810982f658bbb9a4f37ea
arpecop <rudix.lab@gmail.com>
2022-07-10 18:00:06 +0300

    feat(pill): news

--- src/pages/api/joke/[id].ts
+++ src/pages/api/joke/[id].ts
@@ -1,16 +1,28 @@
+import { gql } from '@apollo/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { jokes } from '@/data/nextdb'
+import client from '@/data/client'
+
+const DATA_QUERY = gql`
+  query MyQuery($id: String!) {
+    jokes_by_pk(_id: $id) {
+      _id
+      cat
+      joke
+    }
+  }
+`
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { id } = req.query
-  const joke = await jokes.get(id)
-  if (joke) {
-    res.status(200).json(joke)
-  } else {
-    res.status(404).json({ error: 'Not found' })
-  }
+
+  const { data } = await client.query({
+    query: DATA_QUERY,
+    variables: { id },
+  })
+
+  res.status(200).json(data.jokes_by_pk)
 }
--- src/pages/api/story/[id].ts
+++ src/pages/api/story/[id].ts
@@ -1,17 +1,32 @@
+import { gql } from '@apollo/client'
 import Jimp from 'jimp/es'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import path from 'path'
 
-import { jokes } from '@/data/nextdb'
+import client from '@/data/client'
+
+const DATA_QUERY = gql`
+  query MyQuery($id: String!) {
+    jokes_by_pk(_id: $id) {
+      _id
+      cat
+      joke
+    }
+  }
+`
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { id } = req.query
-  const joke = await jokes.get(id)
 
-  if (joke) {
+  const { data } = await client.query({
+    query: DATA_QUERY,
+    variables: { id },
+  })
+
+  if (data.jokes_by_pk.joke) {
     const image = new Jimp(200, 200, '#232427')
 
     // client.photos.curated({ per_page: 1 }).then((photos) => {
@@ -20,7 +35,7 @@ export default async function handler(
         // return background
         Jimp.loadFont(`${path.resolve('./public')}/font.fnt`)
           .then((font) => {
-            const splitted = joke.joke.split('\n')
+            const splitted = data.jokes_by_pk.joke.split('\n')
             let h = 50
             splitted.forEach((line: string) => {
               image.print(font, 10, h, line, 180)

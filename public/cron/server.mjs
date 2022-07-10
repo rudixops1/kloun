63735ffbdf94cafb17d2ba399e90f68c801260d8
arpecop <rudix.lab@gmail.com>
2022-07-10 18:06:25 +0300

    feat(pill): news

--- public/cron/server.mjs
+++ public/cron/server.mjs
@@ -1,55 +1,46 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable camelcase */
 import fetch from 'node-fetch'
 
 const token = process.env.FB_TOKEN
-fetch('https://pouchdb.herokuapp.com/jokes/')
-  .then(res => res.json())
-  .then(json => {
-    const { doc_count } = json
-    const skip = Math.floor(Math.random() * doc_count)
-    fetch(
-      `https://klounlol.herokuapp.com/jokes/_all_docs?limit=3&skip=${skip}&include_docs=true`
-    )
-      .then(res => res.json())
-      .then(json1 => {
-        const { id } = json1.rows[0]
-        const child_attachments = json1.rows.map(row => {
-          return {
-            link: `https://kloun.lol/joke/${row.id}/`,
-            name: row.doc.cat,
-            picture: `https://kloun.lol/api/story/${row.id}/`,
-          }
-        })
 
-        console.log(
-          `https://graph.facebook.com/v14.0/me/feed?access_token=${token}`
-        )
-        fetch(
-          `https://graph.facebook.com/v14.0/me/feed?access_token=${token}`,
+fetch(`https://kloun.lol/api/joke/random/`)
+  .then(res => res.json())
+  .then(json1 => {
+    const { id } = json1[0]._id
+    const child_attachments = json1.map(row => {
+      return {
+        link: `https://kloun.lol/joke/${row._id}/`,
+        name: row.cat,
+        picture: `https://kloun.lol/api/story/${row._id}/`,
+      }
+    })
+
+    console.log(
+      `https://graph.facebook.com/v14.0/me/feed?access_token=${token}`
+    )
+    fetch(`https://graph.facebook.com/v14.0/me/feed?access_token=${token}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        link: `https://kloun.lol/joke/${id}/`,
+        og_action_type_id: '383635058339457',
+        og_object_id: '140035382814582',
+        og_icon_id: '202857099862987',
+        child_attachments,
+        actions: [
           {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              link: `https://kloun.lol/joke/${id}/`,
-              og_action_type_id: '383635058339457',
-              og_object_id: '140035382814582',
-              og_icon_id: '202857099862987',
-              child_attachments,
-              actions: [
-                {
-                  link: `https://kloun.lol/joke/${id}/`,
-                  name: 'Прочети',
-                },
-              ],
-            }),
-          }
-        )
-          .then(res => res.json())
-          .then(json => {
-            console.log(json)
-          })
+            link: `https://kloun.lol/joke/${id}/`,
+            name: 'Прочети',
+          },
+        ],
+      }),
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
       })
   })

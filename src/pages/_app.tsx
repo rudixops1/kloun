c0a77471d4c0b0ed824989cc9e48d3cd778da2c9
arpecop <rudix.lab@gmail.com>
2022-07-11 10:48:01 +0300

    feat(pill): first page is index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/global.css'
 
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import NextNProgress from 'nextjs-progressbar'
 import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5476404733919333"
       />
+      <NextNProgress />
       <RecoilRoot>
         <Component {...pageProps} />
       </RecoilRoot>

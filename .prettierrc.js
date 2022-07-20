507089088305fd63a76cc4a88eaaacf01a4c7f7d
arpecop <rudix.lab@gmail.com>
2022-07-20 13:50:06 +0300

    feat(pill): migrate

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,3 @@
 module.exports = {
   singleQuote: true,
-  printWidth: 80,
-  proseWrap: 'always',
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  semi: false,
 }

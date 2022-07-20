5efe9347f057b72b268937d15fc46ea7316ada45
arpecop <rudix.lab@gmail.com>
2022-07-20 04:49:48 +0300

    feat(pill): migrate

--- src/data/client.ts
+++ src/data/client.ts
@@ -1,19 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const GRAPH_URL = 'http://34.242.41.16'
-const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'no-cache',
-  },
-  query: {
-    fetchPolicy: 'no-cache',
-  },
-} as any
+
 const client = new ApolloClient({
   ssrMode: true,
   uri: `${GRAPH_URL}/v1/graphql`,
   cache: new InMemoryCache(),
-  defaultOptions,
 })
 // const client2 = new ApolloClient({
 //   ssrMode: true,

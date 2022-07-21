5ab1061347ca40ac5ac0c0c8a57250038e866b05
arpecop <rudix.lab@gmail.com>
2022-07-22 01:34:55 +0300

    feat(pill): lots of progress

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -6,12 +6,12 @@ function Logo({ title }: { title?: string }) {
       <div className='z-10 flex w-full items-center justify-center pt-2 sm:pt-6'>
         <Image
           src='/logobottom.png'
-          alt=''
+          alt='kloun'
           width={169}
           height={195}
           priority={true}
+          placeholder='empty'
         />
-
         <div className='flex items-center justify-center  overflow-hidden truncate text-3xl font-extrabold md:text-6xl'>
           <h1 className=' text-purple-200'>
             {title && <span className='font-medium'>/{title}</span>}

8df76d78adda92a3a768f064387ebe43790060a4
arpecop <rudix.lab@gmail.com>
2022-07-21 03:49:01 +0300

    feat(pill): images

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ module.exports = {
 
   // SVGR
 
+  experimental: {
+    images: {
+      allowFutureImage: true
+    }
+  },
+
   webpack (config) {
     config.module.rules.push({
       test: /\.svg$/i,

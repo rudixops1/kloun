26f4ef1fad270eea6b1ce73abb6f1291da7bddbe
arpecop <rudix.lab@gmail.com>
2022-07-21 21:56:14 +0300

    feat(pill): images

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,26 +3,61 @@ import Link from 'next/link';
 export interface Cat {
   cat: string;
   count: number;
+  altcount?: number;
+  althref?: string;
 }
 export interface NavProps {
   cats: Cat[];
   limit?: number;
+  prefix: string;
+  formatlength?: boolean;
 }
 
-const Nav = ({ cats, limit }: NavProps) => {
+const Nav = ({ cats, limit, prefix, formatlength }: NavProps) => {
   const newcats = cats.slice(0, limit || cats.length);
+  const formatcount = (count: number) => {
+    if (count > 1000) {
+      return `${Math.floor(count / 1000)}k`;
+    }
+    return count;
+  };
+
   return (
     <div className='flex flex-wrap sm:mx-auto sm:mb-2 '>
-      {newcats.map((d: { cat: string; count: number }) => (
-        <Link key={d.cat} href={`/cat/${d.cat.replace(/ /g, '%20')}/`} passHref>
-          <a className='btn1'>
-            <div className='btn1-wrap'>
-              <div className='flex-1 font-medium text-white'> {d.cat}</div>
-              {d.count !== 0 && <div className='btn1-count'>{d.count}</div>}
-            </div>
-          </a>
-        </Link>
-      ))}
+      {newcats.map(
+        (d: {
+          cat: string;
+          count: number;
+          altcount?: number;
+          althref?: string;
+        }) => (
+          <Link
+            key={d.cat}
+            href={`/${prefix}/${d.althref || d.cat.replace(/ /g, '%20')}/`}
+            passHref
+          >
+            <a className='w-full grow p-2 sm:w-1/2 md:w-1/3   lg:w-1/4 xl:w-1/5 '>
+              <div className='flex h-full items-center rounded bg-gray-800 p-4'>
+                <div className='flex-1  justify-center align-middle font-medium text-white'>
+                  {formatlength ? (
+                    <span className='text-xs'>{d.cat}</span>
+                  ) : (
+                    d.cat
+                  )}
+                </div>
+                {d.count !== 0 && (
+                  <div className='btn1-count'>{formatcount(d.count)}</div>
+                )}
+                {d.altcount !== undefined && (
+                  <div className='flex-none  rounded bg-green-700 px-2 text-2xl font-extralight'>
+                    {d.altcount}
+                  </div>
+                )}
+              </div>
+            </a>
+          </Link>
+        )
+      )}
     </div>
   );
 };
--- src/pages/business/index.tsx
+++ src/pages/business/index.tsx
@@ -4,11 +4,20 @@
 import { gql } from '@apollo/client';
 import type { GetServerSideProps } from 'next';
 
-import type { Cat } from '@/components/Nav';
-import Nav from '@/components/Nav';
 import { Main } from '@/components/Layouts/Main';
 import { Meta } from '@/components/Layouts/Meta';
+import type { Cat } from '@/components/Nav';
+import Nav from '@/components/Nav';
 import client from '@/data/client';
+
+export type Company = {
+  _id: string;
+  name: string;
+  zip: number;
+  location: string;
+  uid?: string;
+  created_at: number;
+};
 
 const Index = ({ cats }: { cats: Cat[] }): JSX.Element => {
   return (
@@ -16,14 +25,14 @@ const Index = ({ cats }: { cats: Cat[] }): JSX.Element => {
       hideFooter
       meta={
         <Meta
-          title='Новини'
-          description='Новини'
-          cat='Новини'
-          url='https://www.kloun.lol/news/'
+          title='Бизнес фирми'
+          description='Бизнес фирми'
+          cat='Business'
+          url='https://www.kloun.lol/business/'
         />
       }
     >
-      <Nav cats={cats} prefix='business' />
+      <Nav cats={cats} prefix='business' limit={50} />
       <div className='my-10 flex w-full flex-col'>
         <div className='flex flex-wrap'></div>
       </div>
@@ -31,10 +40,37 @@ const Index = ({ cats }: { cats: Cat[] }): JSX.Element => {
   );
 };
 export const DATA_AGREGATE = gql`
-  query MyQuery {
-    companies_count(order_by: { count: desc }, limit: 100) {
+  query MyQuery @cached {
+    companies_count(order_by: { count: desc }) {
       count
       cat: location
+    }
+  }
+`;
+
+export const CITY_DATA = gql`
+  query MyQuery($location: String!) {
+    cats: companies(
+      limit: 30
+      offset: 1
+      where: { location: { _eq: $location } }
+      order_by: { uid: asc }
+    ) {
+      _id
+      cat: name
+      althref: _id
+      altcount: created_at
+    }
+  }
+`;
+
+export const COMPANY_INFO = gql`
+  query MyQuery($id: Int = 10) {
+    companies_by_pk(_id: $id) {
+      created_at
+      location
+      name
+      zip
     }
   }
 `;

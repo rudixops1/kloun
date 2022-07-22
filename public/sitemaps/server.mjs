62e054a89e05a7d5ad12370f0ec8e004e062fc9d
arpecop <rudix.lab@gmail.com>
2022-07-22 08:46:22 +0300

    feat(pill): missing package

--- public/sitemaps/server.mjs
+++ public/sitemaps/server.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import fetch from 'cross-fetch';
 import fs from 'fs';
 import pkg from 'lodash';
@@ -14,17 +15,22 @@ fetch('http://db.kloun.lol/api/rest/others/structure/sitemaps')
           .fill(0)
           .map(
             (_, i) =>
-              `<loc>https://www.kloun.lol/cat/${item.cat.replace(
+              `<url><loc>https://www.kloun.lol/cat/${item.cat.replace(
                 / /g,
                 '%20'
-              )}/${i + 1}/</loc>`
+              )}/${i +
+                1}/</loc><lastmod>2022-03-20</lastmod><changefreq>daily</changefreq><priority>1.0</priority></url>`
           )
       )
       .flat();
 
     const newsmap = new Array(Math.round(Number(news.aggregate.count) / 30))
       .fill(0)
-      .map((_, i) => `<loc>https://www.kloun.lol/news/${i + 1}/</loc>`);
+      .map(
+        (_, i) =>
+          `<url><lastmod>2022-03-20</lastmod><changefreq>daily</changefreq><priority>1.0</priority><loc>https://www.kloun.lol/news/${i +
+            1}/</loc></url>`
+      );
 
     const businessmap = business
       .map((item) =>
@@ -32,28 +38,25 @@ fetch('http://db.kloun.lol/api/rest/others/structure/sitemaps')
           .fill(0)
           .map(
             (_, i) =>
-              `<loc>https://www.kloun.lol/business/${item.cat.replace(
+              `<url><lastmod>2022-03-20</lastmod><changefreq>daily</changefreq><priority>1.0</priority><loc>https://www.kloun.lol/business/${item.cat.replace(
                 / /g,
                 '%20'
-              )}/${i + 1}/</loc>`
+              )}/${i + 1}/</loc></url>`
           )
       )
       .flat();
 
     const sitemap = chunk(
       shuffle([...newsmap, ...jokesmap, ...businessmap]),
-      500
+      1000
     );
 
     sitemap.forEach(async (element, i) => {
       fs.writeFileSync(
         `/Users/rudix/Desktop/kloun/public/sitemaps/out/sitemap${i + 1}.xml`,
-        `<?xml version="1.0" encoding="UTF-8"?>
-      <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <sitemap>
-      ${element.join('\n')}
-      </sitemap>
-      </sitemapindex>`
+        `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${element.join(
+          ''
+        )}</urlset></xml>`
       );
     });
 

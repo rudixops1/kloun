48713b837259ac542ff4a824ff53984baae01269
arpecop <rudix.lab@gmail.com>
2022-07-23 22:36:29 +0300

    feat(pill): sls

---
+++ src/pages/other/contact.tsx
@@ -0,0 +1,22 @@
+import { Main } from '@/components/Layouts/Main';
+import { Meta } from '@/components/Layouts/Meta';
+
+const Index = (): JSX.Element => {
+  return (
+    <Main hideFooter meta={<Meta title='Contact' description='Contact' />}>
+      <h1 className='text-center text-3xl font-thin'>Contact</h1>
+      <div className='mt-10 flex flex-row items-center justify-center'>
+        <div className='mb-8 w-full px-3 lg:mb-0 lg:w-1/5'>
+          <p className='mb-2 font-bold lg:mb-4 lg:text-lg'>Office</p>
+          <p className='lg:text-lg'>14 Parijka Komuna, Varna, Bulgaria</p>
+        </div>
+        <div className='mb-8 w-full px-3 lg:mb-0 lg:w-1/5'>
+          <p className='mb-2 font-bold lg:mb-4 lg:text-lg'>Contacts</p>
+          <p className='lg:text-lg'>(+359) 876 358 115 info@rudixops.com</p>
+        </div>
+      </div>
+    </Main>
+  );
+};
+
+export default Index;
---
+++ src/pages/other/terms.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+import { Main } from '@/components/Layouts/Main';
+import { Meta } from '@/components/Layouts/Meta';
+
+const Index = (): JSX.Element => {
+  return (
+    <Main hideFooter meta={<Meta title='Terms of Use' description='Terms' />}>
+      <Image
+        src='/rudixops.png'
+        width={171}
+        height={40}
+        alt=''
+        objectFit='contain'
+      />
+      <p className='mt-10'>
+        RudixOps не носи отговорност за настъпили вреди или пропуснати ползи от
+        поместената в сайта информация или от нейното неправилно
+        възприемане/тълкуване.
+      </p>
+      <p>
+        Ние се стремим да направим този сайт максимално лесен за работа, но не
+        гарантираме и не носим отговорност за неговата непрекъсната работа; за
+        несъвместимост с определени компютърни програми; за технически грешки;
+        за невъзможност за достъп до сайта поради причини извън нашата
+        компетенция и други.
+      </p>
+    </Main>
+  );
+};
+
+export default Index;

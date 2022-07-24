3218f53c01a4e2499a5c4d977cd518ad611ec458
arpecop <rudix.lab@gmail.com>
2022-07-25 00:25:03 +0300

    feat(pill): changed to cross fetch

--- src/data/client.ts
+++ src/data/client.ts
@@ -1,20 +1,21 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import fetch from 'cross-fetch';
 
 export const GRAPH_URL = 'http://54.247.0.27';
 
-const client = new ApolloClient({
-  ssrMode: true,
+// const client = new ApolloClient({
+//   ssrMode: true,
+//   uri: `${GRAPH_URL}/v1/graphql`,
+//   cache: new InMemoryCache(),
+// });
+const httpLink = new HttpLink({
   uri: `${GRAPH_URL}/v1/graphql`,
+  fetch,
+});
+const client = new ApolloClient({
+  ssrMode: false,
+  link: httpLink,
   cache: new InMemoryCache(),
 });
-// const client2 = new ApolloClient({
-//   ssrMode: true,
-//   link: new HttpLink({
-//     uri: `${GRAPH_URL}/v1/graphql`,
-//     fetch,
-//   }),
-//   cache: new InMemoryCache(),
-//   defaultOptions,
-// })
 
 export default client;

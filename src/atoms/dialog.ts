47df4a2fe276a325efe2d5e7c97e4d61203f5105
arpecop <rudix.lab@gmail.com>
2022-07-26 15:11:34 +0300

    feat(pill): login start

--- src/atoms/dialog.ts
+++ src/atoms/dialog.ts
@@ -4,3 +4,15 @@ export const dialogAtom = atom({
   key: new Date().toString(), // unique ID (with respect to other atoms/selectors)
   default: {}, // default value (aka initial value)
 });
+
+export const loggedInUserData = atom({
+  key: `${new Date().toString()}1`,
+  default: {
+    sub: null,
+    email_verified: null,
+    phone_number_verified: null,
+    email: null,
+    username: null,
+    token: null,
+  },
+});
